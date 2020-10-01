64c6b4b15e424015a803b1de9d1d539e99830fb0
Jeff T <jeff.thao92@gmail.com>
2020-09-30 22:37:15 -0500

    added moment.js script and removed day.js script, added var now to call moment function and format it to show date and time. Declared var currentTime to get #currentDay in html. then add date and time into website. added margin-top into #currentDay, made it bold and increased font-size to 24px.

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-var currentTime = "";
+var currentTime = document.getElementById("currentDay");
 
+var now = moment().format('LLLL');
+console.log(now);
+currentTime.textContent = now;
 
 $(document).ready(function() {
 
 console.log("hello")
+
+
+
+
 
 });
