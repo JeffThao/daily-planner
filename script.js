fa9af0708b1b8c1de7dc6f6954217c489d658d43
Jeff T <jeff.thao92@gmail.com>
2020-10-01 23:02:26 -0500

    removed divs in HTML, restyled in js. added if statement to style as past, present or future. added array for hours, added another var for moment.js to be called in as military time. Added if condition to change time displayed in .hour as am or pm. Added click function on .saveBtn, made it also save to localStorage.

--- script.js
+++ script.js
@@ -1,22 +1,73 @@
 var currentTime = document.getElementById("currentDay");
 var now = moment().format("llll");
 currentTime.textContent = now;
+var currentTime = moment().format("HH");
 
-var toDo = document.getElementsByTagName("textarea");
-
-toDo = 
-
+var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 
-$(document).ready(function() {
-console.log("hello")
+function makeBlocks() {
+    for (var i = 0; i < time.length; i++) {
+        var d1 = document.createElement("div");
 
-$(".saveBtn").on("click",function(){
+        d1.setAttribute("class", "row")
+        var d2 = document.createElement("div");
+        d2.setAttribute("class", "col-1 hour")
 
-    console.log("I've been clicked");
-})
+        if (time[i] > 12) {
+            d2.textContent = time[i] - 12 + " PM";
 
+        }
+        if (time[i] == 12) {
+            d2.textContent = time[i] + " PM";
 
+        }
+        if (time[i] < 12) {
+            d2.textContent = time[i] + " AM";
 
+        }
+        var textarea = document.createElement("textarea");
 
-});
+        if (currentTime == time[i]) {
+            textarea.setAttribute("class", "present col-10 time-block " + time[i]);
+        }
+        if (currentTime > time[i]) {
+            textarea.setAttribute("class", "past col-10 time-block " + time[i]);
+        }
+        if (currentTime < time[i]) {
+            textarea.setAttribute("class", "future col-10 time-block " + time[i]);
+        }
+
+        var d3 = document.createElement("div");
+        d3.setAttribute("class", "col-1 saveBtn");
+        d3.setAttribute("id", time[i]);
+        var itag = document.createElement("i");
+        itag.setAttribute("class", "col-12 far fa-save");
+
+        d3.appendChild(itag);
+
+        d1.appendChild(d2);
+        d1.appendChild(textarea);
+        d1.appendChild(d3);
+
+        document.querySelector(".container").appendChild(d1);
+    }
+
+    $(".saveBtn").on("click", function () {
+
+        console.log(this.getAttribute("id"));
+        console.log(this.parentElement.children[1].value);
+        var idTime = this.getAttribute("id");
+        var textAreaContent = this.parentElement.children[1].value;
+        localStorage.setItem(idTime, textAreaContent);
+    });
+
+    function getToDos() {
+        var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+    }
+    
+}
+
+makeBlocks();
+
