a011fa7006f92113f0f7c3b58a32008f0a065ceb
Jeff T <jeff.thao92@gmail.com>
2020-10-02 10:04:27 -0500

    Added .ready function in script.js.

--- script.js
+++ script.js
@@ -5,68 +5,69 @@ var currentTime = moment().format("HH");
 
 var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+$(document).ready(function () {
 
-function makeBlocks() {
-    for (var i = 0; i < time.length; i++) {
-        var d1 = document.createElement("div");
+    function makeBlocks() {
+        for (var i = 0; i < time.length; i++) {
+            var d1 = document.createElement("div");
 
-        d1.setAttribute("class", "row")
-        var d2 = document.createElement("div");
-        d2.setAttribute("class", "col-1 hour")
+            d1.setAttribute("class", "row")
+            var d2 = document.createElement("div");
+            d2.setAttribute("class", "col-1 hour")
 
-        if (time[i] > 12) {
-            d2.textContent = time[i] - 12 + " PM";
+            if (time[i] > 12) {
+                d2.textContent = time[i] - 12 + " PM";
 
+            }
+            if (time[i] == 12) {
+                d2.textContent = time[i] + " PM";
+
+            }
+            if (time[i] < 12) {
+                d2.textContent = time[i] + " AM";
+
+            }
+            var textarea = document.createElement("textarea");
+            textarea.setAttribute("placeholder", "Plan your day here.");
+
+            if (currentTime == time[i]) {
+                textarea.setAttribute("class", "present col-10 time-block " + time[i]);
+            }
+            if (currentTime > time[i]) {
+                textarea.setAttribute("class", "past col-10 time-block " + time[i]);
+            }
+            if (currentTime < time[i]) {
+                textarea.setAttribute("class", "future col-10 time-block " + time[i]);
+            }
+
+            var d3 = document.createElement("div");
+            d3.setAttribute("class", "col-1 saveBtn");
+            d3.setAttribute("id", time[i]);
+            var itag = document.createElement("i");
+            itag.setAttribute("class", "col-12 far fa-save");
+
+            d3.appendChild(itag);
+
+            d1.appendChild(d2);
+            d1.appendChild(textarea);
+            d1.appendChild(d3);
+
+            document.querySelector(".container").appendChild(d1);
         }
-        if (time[i] == 12) {
-            d2.textContent = time[i] + " PM";
 
-        }
-        if (time[i] < 12) {
-            d2.textContent = time[i] + " AM";
+        $(".saveBtn").on("click", function () {
+            var idTime = this.getAttribute("id");
+            var textAreaContent = this.parentElement.children[1].value;
+            localStorage.setItem(idTime, textAreaContent);
+        });
 
-        }
-        var textarea = document.createElement("textarea");
-        textarea.setAttribute("placeholder", "Plan your day here.");
-
-        if (currentTime == time[i]) {
-            textarea.setAttribute("class", "present col-10 time-block " + time[i]);
-        }
-        if (currentTime > time[i]) {
-            textarea.setAttribute("class", "past col-10 time-block " + time[i]);
-        }
-        if (currentTime < time[i]) {
-            textarea.setAttribute("class", "future col-10 time-block " + time[i]);
+        function getToDos() {
+            var time = d3.setAttribute("id", time[i]);
+            var storedTodos = JSON.parse(localStorage.getItem(time));
         }
 
-        var d3 = document.createElement("div");
-        d3.setAttribute("class", "col-1 saveBtn");
-        d3.setAttribute("id", time[i]);
-        var itag = document.createElement("i");
-        itag.setAttribute("class", "col-12 far fa-save");
-
-        d3.appendChild(itag);
-
-        d1.appendChild(d2);
-        d1.appendChild(textarea);
-        d1.appendChild(d3);
-
-        document.querySelector(".container").appendChild(d1);
     }
 
-    $(".saveBtn").on("click", function () {
-        var idTime = this.getAttribute("id");
-        var textAreaContent = this.parentElement.children[1].value;
-        localStorage.setItem(idTime, textAreaContent);
-    });
+    makeBlocks();
 
-    function getToDos() {
-        var time = d3.setAttribute("id", time[i]);
-        var storedTodos = JSON.parse(localStorage.getItem(time));
-    }
-    
-}
-
-makeBlocks();
-
-
+});
